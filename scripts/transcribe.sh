fe1fbe301ad9caa5f8d595781bc9d873bf116eb2
yakov <61390950+thewh1teagle@users.noreply.github.com>
2026-04-10 20:45:45 +0300

    create dataset and upload

--- scripts/transcribe.sh
+++ scripts/transcribe.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/home/yakov/Documents/spark-docs/CTranslate2/build${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 uv run python -m src.run "$@"
+uv run hf repo create knesset-ipa --type dataset || true
+uv run hf upload knesset-ipa ./output.tsv
